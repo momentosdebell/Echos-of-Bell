7eef8317607d7ee72e33d7a5fa414fea4d6cf74b
momentosdebell <isabellemlinden@gmail.com>
2026-08-28 02:30:45 +0200

    vault backup: 2026-08-28 02:30:44

--- quartz/components/Body.tsx
+++ quartz/components/Body.tsx
@@ -1,19 +1,7 @@
 import { QuartzComponent, QuartzComponentConstructor, QuartzComponentProps } from "./types"
 
-const Body: QuartzComponent = ({ children, fileData }: QuartzComponentProps) => {
-  const rawImg = fileData.frontmatter?.img
-  const cleanImgName = rawImg ? String(rawImg).replace(/[\[\]"]/g, "").trim() : null
-
-  return (
-    <div id="quartz-body">
-      {cleanImgName && (
-        <div style="float: right; margin: 0 0 1rem 1.5rem; max-width: 300px;">
-          <img src={`/static/${cleanImgName}`} alt="" style="border-radius: 6px; width: 100%; display: block;" />
-        </div>
-      )}
-      {children}
-    </div>
-  )
+const Body: QuartzComponent = ({ children }: QuartzComponentProps) => {
+  return <div id="quartz-body">{children}</div>
 }
 
 export default (() => Body) satisfies QuartzComponentConstructor
